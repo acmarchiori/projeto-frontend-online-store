ef79e470c4a736c40284e8160bd251b59d2cca8a
Nathaniel <naniidalpozzo@gmail.com>
2023-02-09 15:36:44 -0300

    iniciando projeto

--- src/index.js
+++ src/index.js
@@ -5,13 +5,3 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-export async function getCategories() {
-  // Implemente aqui
-}
-
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-  // Essa função será chamada em vários momentos do projeto para buscar os produtos pela categoria e querys
-  // Dentro da função você pode usar condicionais definir qual API utilizar
-}
