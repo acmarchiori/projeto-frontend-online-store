bbcbec6a44c833a0b3933d242505944f61ccb096
acmarchiori <ac_marchiori@yahoo.com.br>
2023-02-10 17:15:28 -0300

    Finalizado requisito 5

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProductList extends Component {
+  list = () => {
+    const { list } = this.props;
+
+    if (list.length <= 0) {
+      // console.log(list);
+      // console.log('entrei no if');
+      return (
+        <p>Nenhum produto foi encontrado</p>
+      );
+    }
+    // console.log('entrei no else');
+    // console.log(list);
+    return (
+      list.map((produto) => (
+        <li data-testid="product" key={ produto.category_id }>
+          <p>{ produto.title }</p>
+          <img src={ produto.thumbnail } alt={ produto.title } />
+          <p>{ produto.price }</p>
+        </li>
+      )));
+  };
+
+  render() {
+    return (
+      <div>
+        {
+          this.list()
+        }
+      </div>
+    );
+  }
+}
+
+ProductList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default ProductList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ListCategories from '../components/ListCategories';
+import ProductList from '../components/ProductList';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
+  state = {
+    query: '',
+    queryList: [],
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  callList = async () => {
+    const { query, queryList } = this.state;
+    const list = await getProductsFromCategoryAndQuery('', query);
+    this.setState({ queryList: list.results });
+    console.log(queryList);
+    return list;
+  };
+
   render() {
+    const { query, queryList } = this.state;
     return (
       <div>
-        <input type="text" name="" id="" />
+        <input
+          data-testid="query-input"
+          type="text"
+          name="query"
+          value={ query }
+          id="query"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.callList }
+        >
+          Pesquisa
+        </button>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
         <Link data-testid="shopping-cart-button" to="/Cart">Carrinho de compras</Link>
         <ListCategories />
+        <ProductList list={ queryList } />
       </div>
     );
   }
