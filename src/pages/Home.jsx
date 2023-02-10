068140b79d722207c8691b87d60549648fab7b84
acmarchiori <ac_marchiori@yahoo.com.br>
2023-02-10 15:17:07 -0300

    Finalizado requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ListCategories from '../components/ListCategories';
 
 class Home extends Component {
   render() {
@@ -9,11 +10,9 @@ class Home extends Component {
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
-
         <Link data-testid="shopping-cart-button" to="/Cart">Carrinho de compras</Link>
-
+        <ListCategories />
       </div>
-
     );
   }
 }
